5ef1fc941669f0fe704cad5978f8e4768d46b17e
Danilo Pomari <danilopomari@gmail.com>
2025-11-30 21:19:33 -0300

    :books: 19 window function

--- 19_window_function.sql
+++ 19_window_function.sql
@@ -13,9 +13,20 @@ WITH tb_clientes_dia AS (
 
     GROUP BY idCliente, dtDia
 
+),
+
+tb_lag AS (
+
+        SELECT *,
+                sum(qtdeTransacao) OVER (PARTITION BY IdCliente ORDER BY dtDIa) AS acum,
+                lag(qtdeTransacao) OVER (PARTITION BY idCliente ORDER BY dtDia) AS lagTransacao
+
+        FROM tb_clientes_dia
+
 )
 
 SELECT *,
-        sum(qtdeTransacao) OVER (PARTITION BY IdCliente ORDER BY dtDIa) AS acum,
-        lag(qtdeTransacao) OVER (PARTITION BY idCliente ORDER BY dtDia) AS lagTransacao
-FROM tb_clientes_dia
+        1. *qtdeTransacao / lagTransacao
+
+FROM tb_lag
+
